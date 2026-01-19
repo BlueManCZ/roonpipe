11f6f59dc327c55d5dc333fb0369cd5151c88ca5
BlueManCZ <ivo97@centrum.cz>
2026-01-19 20:28:14 +0100

    SWC for TypeScript compilation and minification

--- scripts/postbuild.ts
+++ scripts/postbuild.ts
@@ -2,10 +2,8 @@ import fs from "node:fs";
 import path from "node:path";
 
 const file = path.join(__dirname, "..", "dist", "index.js");
-const shebang = "#!/usr/bin/env node\n";
-
 const content = fs.readFileSync(file, "utf8");
 
 if (!content.startsWith("#!")) {
-    fs.writeFileSync(file, shebang + content);
+    fs.writeFileSync(file, `#!/usr/bin/env node\n${content}`);
 }
